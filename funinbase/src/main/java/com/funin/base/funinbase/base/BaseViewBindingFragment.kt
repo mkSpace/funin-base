3498cb17f2c1561a1179b738090a7ec1ed234926
Jaemin.Park <once29@naver.com>
2021-02-25 00:16:55 +0900

    Implement BaseViewBindingFragment

---
+++ funinbase/src/main/java/com/funin/base/funinbase/base/BaseViewBindingFragment.kt
@@ -0,0 +1,29 @@
+package com.funin.base.funinbase.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseViewBindingFragment<VB : ViewBinding> : BaseViewModelFragment() {
+
+    private var _binding: VB? = null
+    val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = setBinding(inflater, container)
+        return binding.root
+    }
+
+    abstract fun setBinding(inflater: LayoutInflater, container: ViewGroup?): VB
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
